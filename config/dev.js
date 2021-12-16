80f23e4139c776a1e4a5546512e4c33ea678df68
zhengxiuyue <zhengxiuyue@jd.com>
2021-12-17 02:04:19 +0800

    feat: 环境变量

--- config/dev.js
+++ config/dev.js
@@ -1,11 +1,14 @@
+process.env.APP_ENV = process.env.APP_ENV || 'dev'
 
-require('dotenv-flow').config()
-console.log(process.env)
+require('dotenv-flow').config({
+  node_env: process.env.APP_ENV
+})
 
 module.exports = {
   env: {
-    NODE_ENV: process.env.NODE_ENV,
-    APP_VERSION: process.env.npm_package_version
+    APP_VERSION: process.env.npm_package_version,
+    APP_ENV: process.env.APP_ENV,
+    APP_API: process.env.APP_API
   },
   defineConstants: {},
   mini: {},
--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,14 @@
+process.env.APP_ENV = process.env.APP_ENV || 'dev'
+
+require('dotenv-flow').config({
+  node_env: process.env.APP_ENV
+})
+
 module.exports = {
   env: {
-    NODE_ENV: '"production"',
+    APP_VERSION: process.env.npm_package_version,
+    APP_ENV: process.env.APP_ENV,
+    APP_API: process.env.APP_API
   },
   defineConstants: {},
   mini: {},
