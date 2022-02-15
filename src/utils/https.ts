8c70c45249dc553cbf0a17d528370bcbd2e84a18
zhengxiuyue <zhengxiuyue@jd.com>
2022-02-15 20:22:16 +0800

    feat: store

--- src/utils/https.ts
+++ src/utils/https.ts
@@ -1,22 +1,22 @@
-import { request } from "@tarojs/taro";
+import { request } from '@tarojs/taro'
 
 // 请求头类型
 const contentTypes = {
-    json: 'application/json; charset=utf-8',
-    urlencoded: 'application/x-www-form-urlencoded; charset=utf-8',
-    multipart: 'multipart/form-data',
+  json: 'application/json; charset=utf-8',
+  urlencoded: 'application/x-www-form-urlencoded; charset=utf-8',
+  multipart: 'multipart/form-data',
 }
 
 // 默认参数
 const defaultOptions = {
-    timeout: 3000,
-    credentials: "include", // 设置H5端携带Cookie
-    dataType: "json" // 返回的数据结构
+  timeout: 3000,
+  credentials: 'include', // 设置H5端携带Cookie
+  dataType: 'json' // 返回的数据结构
 }
 
 interface OptionParams {
     url: string,
-    method?: "OPTIONS" | "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "TRACE" | "CONNECT",
+    method?: 'OPTIONS' | 'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'TRACE' | 'CONNECT',
     data?: object,
     options?: any,
     contentType: 'json' | 'urlencoded' | 'multipart',
@@ -24,55 +24,53 @@ interface OptionParams {
 }
 
 const callApi = ({
-    url,
-    method = 'GET',
-    data,
-    options,
-    contentType = 'json',
-    prefixUrl = 'api'
+  url,
+  method = 'GET',
+  data,
+  options,
+  contentType = 'json',
+  prefixUrl = 'api'
 }: OptionParams) => {
-    if (!url) {
-        const error = new Error('请传入url')
-        return Promise.reject(error)
-    }
+  if (!url) {
+    const error = new Error('请传入url')
+    return Promise.reject(error)
+  }
 
-    const fullUrl = `/${prefixUrl}/${url}`
+  const fullUrl = `/${prefixUrl}/${url}`
 
-    const newOptions = {
-        ...defaultOptions,
-        header: {
-            'content-type':
-                (options.header && options.header['content-type']) ||
-                contentTypes[contentType],
-            'X-Requested-With': 'XMLHttpRequest',
-        },
-        ...options,
-        data,
-        method
-    }
+  const newOptions = {
+    ...defaultOptions,
+    header: {
+      'content-type': options?.header['content-type'] || contentTypes[contentType],
+      'X-Requested-With': 'XMLHttpRequest',
+    },
+    ...options,
+    data,
+    method
+  }
 
-    return request({
-        url: fullUrl,
-        ...newOptions
+  return request({
+    url: fullUrl,
+    ...newOptions
+  })
+    .then((response) => {
+      const { data } = response
+      if (data.code === 'xxx') {
+        // 与服务端约定
+        // 登录校验失败
+      } else if (data.code === 'xxx1') {
+        // 与服务端约定
+        // 无权限
+      } else if (data.code === 'xxx2') {
+        // 与服务端约定
+        return Promise.resolve(data)
+      } else {
+        return Promise.reject(data)
+      }
     })
-        .then((response) => {
-            const { data } = response
-            if (data.code === 'xxx') {
-                // 与服务端约定
-                // 登录校验失败
-            } else if (data.code === 'xxx') {
-                // 与服务端约定
-                // 无权限
-            } else if (data.code === 'xxx') {
-                // 与服务端约定
-                return Promise.resolve(data)
-            } else {
-                return Promise.reject(data)
-            }
-        })
-        .catch((error) => {
-            return Promise.reject(error)
-        })
+    .catch((error) => {
+      return Promise.reject(error)
+    })
 
 }
 
